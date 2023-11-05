abc01e3a06718d2900c6c98813ed3b265bc1d8fc
Yuewen Sun <ysun755@aucklanduni.ac.nz>
2023-11-05 21:13:57 +1300

    Added MainPage.jsx and shopping cart sub-directory. Modified App.jsx and App.css.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; 
+import MainPage from './MainPage'; // Import MainPage component
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
