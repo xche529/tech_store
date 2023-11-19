d6b9564352517874118a9efb9a8d20f805806b46
Yuewen Sun <ysun755@aucklanduni.ac.nz>
2023-11-19 20:42:47 +1300

    Initalized firebase config.

---
+++ src/firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from '@firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyADtL-xpLP05D6NRTFE9UKxrD7pI1BUREk",
+  authDomain: "tech-store-68146.firebaseapp.com",
+  projectId: "tech-store-68146",
+  storageBucket: "tech-store-68146.appspot.com",
+  messagingSenderId: "552981338221",
+  appId: "1:552981338221:web:d46f4c1cb310cdca298764",
+  measurementId: "G-30CC3YCJXN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
