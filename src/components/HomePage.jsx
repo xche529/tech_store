841e481d9ed35473154872c7d46734c9136cd881
xche529 <126313171+xche529@users.noreply.github.com>
2023-11-20 15:42:29 +1300

    Added hide function

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SeaCucumber from '../images/SeaCucumber.jpg';
 import ShowOffButton from './ShowOffButton';
 import '../css/homePage.css';
 
 
 function HomePage() {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const hide = () => {
+        setIsVisible(false);
+    };
+
+    const show = () => {
+        setIsVisible(true);
+    };
+
+    return (
+        <div>
+            {isVisible && content()}
+            <button onClick={hide}>hide</button>
+            <button onClick={show}>show</button>
+        </div>
+    );
+}
+
+function content() {
+
     const handleButtonClick = () => {
         console.log('SeaCucumber clicked!');
     };
+
     return (
         <div className='main'>
-            
+
             {/* temporary list for effect */}
             <ShowOffButton src={SeaCucumber} alt="meme" onClick={handleButtonClick} name={'海参'} price={80} />
             <ShowOffButton src={SeaCucumber} alt="meme" onClick={handleButtonClick} name={'海参'} price={90} />
