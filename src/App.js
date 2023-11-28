7b9d4627a5043d51200dd1e975df5e392a5daacd
Yuewen Sun <ysun755@aucklanduni.ac.nz>
2023-11-28 16:51:59 +1300

    Fixed duplicate import in App.js.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef} from 'react';
+import React, { useRef } from 'react';
 import './index.css';
 import './css/cart.css';
 import { withRouter, BrowserRouter as Router, Route, Routes} from "react-router-dom";
