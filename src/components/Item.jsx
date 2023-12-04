b02e90ef593a957f749aa3de589d45a8964e42f4
xche529 <126313171+xche529@users.noreply.github.com>
2023-12-04 14:27:46 +1300

    Linked item page to home page

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ImageWithFallback from './image';
 import SeaCucumber from '../images/SeaCucumber.jpg';
+import '../css/item.css';
 
 
-function Item(product) {
+
+function Item(props) {
+
+  const { product } = props;
+
   return (
     <div>
       <ImageWithFallback className='image'
         src={product.imageUrl}
-        fallbackSrc={SeaCucumber}
         alt={"Seacucumber"}
       />
-      <div>{product.name}</div>
-      <div>{product.description}</div>
-      <div>{product.price}</div>
+      <div className='infoBox'>
+      <div>product name: {product.name}</div>
+      <div>description: {product.description}</div>
+      <div>price: {product.price}</div>
+      </div>
 
     </div>
   );
