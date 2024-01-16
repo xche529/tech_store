623df16f452420caf94f9c15af4f7104f9a09df6
xche529 <126313171+xche529@users.noreply.github.com>
2024-01-16 13:45:13 +1300

    Added signin with google account

--- src/components/account/LogIn.jsx
+++ src/components/account/LogIn.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useAuth } from '../.././context/authContext';
 
 
 import '../../css/logIn.css';
 
 function LogIn() {
+  const provider = new GoogleAuthProvider();
   const { login } = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -38,6 +39,23 @@ function LogIn() {
     console.log(`Password: ${password}`);
   };
 
+
+  const handleGoogleSignIn = async () => {
+    const auth = getAuth();
+    signInWithPopup(auth,provider).then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      login(user);
+      console.log(user);
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+  }
+
   return (
     <div className="login">
       <h1>Log In</h1>
@@ -50,6 +68,7 @@ function LogIn() {
         <br />
         <button type="submit">Log In </button>
       </form>
+      <button onClick={handleGoogleSignIn}>Sign in with Google</button>
       <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
     </div>
   );
