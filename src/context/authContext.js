9b9a960182bceb95bde71d3bb4fc0929724976e7
xche529 <126313171+xche529@users.noreply.github.com>
2024-02-02 12:12:35 +1300

    Fixed avatar display bug

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,4 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react';
+import { collection, getDocs, getDoc, doc, updateDoc, increment, deleteDoc } from 'firebase/firestore';
+import { db } from '../firebase-config';
 
 const AuthContext = createContext();
 
@@ -7,18 +9,35 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const storedUser = localStorage.getItem('user');
+    const storedUserDetail = localStorage.getItem('userDetail');
+    let tempUser = null;
     if (storedUser) {
-      setUser(JSON.parse(storedUser));
+      tempUser = JSON.parse(storedUser)
+      if(storedUserDetail != undefined){
+        console.log("storedUserDetail:", storedUserDetail)
+        tempUser.avatar = (storedUserDetail)
+      }
+      setUser(tempUser);
     }
+    console.log("Read User:", tempUser)  
   }, []);
 
-  useEffect(() => {
-    showResult();
-  }, [user]);
-
-  const login = (userData) => {
-    setUser(userData);
-    localStorage.setItem('user', JSON.stringify(userData));
+  async function login(userData){
+    try{
+      const userDetailRef = collection(db, 'users')
+      const userDetailDoc = doc(userDetailRef, userData.email)
+      console.log("avatarDoc:", userDetailDoc)
+      const userDetail = await getDoc(userDetailDoc)
+      if(userDetail.exists()){
+        userData.avatar = userDetail.data().avatar
+        console.log("avatar:", userData.avatar)
+        localStorage.setItem('userDetail', userData.avatar);
+      }
+      setUser(userData);
+      localStorage.setItem('user', JSON.stringify(userData));
+    }catch(err){
+      console.log(err)
+    }
   };
 
   const showResult = () => {
