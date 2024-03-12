c133bcbac8fafc39ea9baf31f5956d25efcd8a71
Yuewen Sun <ysun755@aucklanduni.ac.nz>
2024-03-12 23:18:59 +1300

    Refactored home page product container with TailwindCSS.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,72 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate, useParams } from 'react-router-dom';
-import ShowOffButton from './ShowOffButton';
-import Item from './Item';
-import '../css/homePage.css';
-import { collection, getDocs, where, query, limit, orderBy } from 'firebase/firestore';
+import { useParams, useNavigate } from 'react-router-dom'; 
+import { collection, getDocs, where, query } from 'firebase/firestore';
 import { db } from '../firebase-config';
+import ShowOffButton from './ShowOffButton';
 
 function HomePage() {
-
-    let productsRef = collection(db, 'products');
-    //main page products list
     const [products, setProducts] = useState([]);
-
     const { keyWordString = "homepage" } = useParams();
-
     const [keyWords, setKeyWords] = useState([]);
 
-    const fetchProducts = async () => {
-        setProducts([])
-        let q = productsRef;
-        let data;
-        console.log('keyWords:', keyWords);
-
-        if (keyWords.length > 0) {
-            q = query(productsRef, where("tag", "array-contains-any", keyWords));
-        }
-        data = await getDocs(q);
-        setProducts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-
-    }
-
     useEffect(() => {
+        const fetchProducts = async () => {
+            setProducts([]);
+            let q = collection(db, 'products');
+            if (keyWords.length > 0) {
+                q = query(q, where("tag", "array-contains-any", keyWords));
+            }
+            const data = await getDocs(q);
+            setProducts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
         fetchProducts();
     }, [keyWords]);
 
-
     useEffect(() => {
         const keyWords = keyWordString ? keyWordString.split(" ") : ["homepage"];
-        let lowerCaseKeyWords = keyWords.map((word) => word.toLowerCase());
-        setKeyWords(lowerCaseKeyWords);
+        setKeyWords(keyWords.map((word) => word.toLowerCase()));
     }, [keyWordString]);
 
-    return Content(products);
-};
-
-function Content(products) {
-    const navigate = useNavigate();
+    const navigate = useNavigate(); 
 
     const handleButtonClick = (product, index) => {
-        navigate('/Item/' + product.id)
+        navigate('/Item/' + product.id);
         console.log('SeaCucumber' + index + 'clicked!');
     };
 
-    // return the list of products
     return (
-        <>
-            <div className='main'>
-                {products.map((product, index) => (
-                    <ShowOffButton
-                        key={index}
-                        alt="seacucumber"
-                        onClick={() => handleButtonClick(product, index)}
-                        product={product}
-                    />
-                ))}
-            </div>
-        </>
+        <div className="main grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-center">
+            {products.map((product, index) => (
+                <ShowOffButton key={index} alt="seacucumber"  onClick={() => handleButtonClick(product, index)} product={product} />
+            ))}
+        </div>
     );
 }
 
 export default HomePage;
+
+
--- src/components/ShowOffButton.jsx
+++ src/components/ShowOffButton.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
-import '../css/showOffButton.css';
 import SeaCucumber from '../images/SeaCucumber.jpg';
 import ImageWithFallback from './image';
 
-
-
 function ShowOffButton({ alt, onClick, product }) {
-    return (
-        <div className="button" onClick={onClick}>
-            <ImageWithFallback className='image'
-                src={product.imageUrl}
-                fallbackSrc={SeaCucumber}
-                alt={alt}
-            />
-            <div className='textInfo'>
-                <div className="mainName">
-                    {product.name}
-                </div>
-                <div className='mainPrice'>
-
-                    {product.price ? '$' + product.price : ''}
-                </div>
-            </div>
-
-            <div className='discription'>
-                {product.description}
-            </div>
-            <div className='id'>
-                ID: {product.id}
-            </div>
+  return (
+    <div className="bg-white w-80 h-100 flex flex-col justify-center items-center m-2 p-2 border border-gray-300 rounded-lg hover:scale-105 duration-300" onClick={onClick}>
+      <div className="w-full flex justify-center items-center">
+        <ImageWithFallback
+          className='w-10/12 h-50 rounded-lg m-2'
+          src={product.imageUrl}
+          fallbackSrc={SeaCucumber}
+          alt={alt}
+        />
+      </div>
+      <div className='p-5 gap-3 flex flex-col justify-center items-center'>
+        <div className="text-xl font-semibold text-center">
+          {product.name}
         </div>
-    );
+        <div className="w-20 h-7 bg-blue-500 flex justify-center items-center text-white font-bold text-md py-0.5 px-2 rounded-full">
+          {product.price ? '$' + product.price : ''}
+        </div>
+      </div>
+      <div className='description text-sm text-gray-600 mb-4'>
+      </div>
+      <div className='id text-xs text-gray-400'>
+        ID: {product.id}
+      </div>
+    </div>
+  );
 }
+
 export default ShowOffButton;
+
+
+
+
+
+
+
+
+
