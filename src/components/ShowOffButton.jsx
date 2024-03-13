8c0571b2af7a1a6b30d211511c6e67e4fb07310b
Yuewen Sun <ysun755@aucklanduni.ac.nz>
2024-03-13 17:48:36 +1300

    Improved layout of product list.

--- src/components/ShowOffButton.jsx
+++ src/components/ShowOffButton.jsx
@@ -14,10 +14,10 @@ function ShowOffButton({ alt, onClick, product }) {
         />
       </div>
       <div className='p-5 gap-3 flex flex-col justify-center items-center'>
-        <div className="text-xl font-semibold text-center">
+        <div className="text-lg font-semibold text-center">
           {product.name}
         </div>
-        <div className="w-20 h-7 bg-blue-500 flex justify-center items-center text-white font-bold text-md py-0.5 px-2 rounded-full">
+        <div className="w-20 h-7 bg-blue-500 flex justify-center items-center text-white text-xl font-bold text-md py-0.5 px-2 rounded-full">
           {product.price ? '$' + product.price : ''}
         </div>
       </div>
--- src/components/admin/ProductList.jsx
+++ src/components/admin/ProductList.jsx
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { collection, getDocs, updateDoc, doc } from 'firebase/firestore';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase-config';
-import { updateImage } from './product-component/UpdateImage';
 import ProductOverlay from './product-component/ProductOverlay';
 import  Search  from './product-component/SearchArea';
-import '../../css/admin.css';
-import '../../css/admin-search.css';
 
 const AdminProductList = () => {
     const [products, setProducts] = useState([]);
-    const [file, setFile] = useState(null);
-    const [previewImage, setPreviewImage] = useState(null);
     const [selectedProduct, setSelectedProduct] = useState(null);
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedCategory, setSelectedCategory] = useState('');
@@ -50,12 +44,12 @@ const AdminProductList = () => {
 
     return (
 
-        <div className="admin-product-list">
+        <div className="admin-product-list flex items-center justify-center">
          <Search onSearch={handleSearch} onCategoryChange={handleCategoryChange}  />
           {filteredProducts.map((product) => (
-            <div key={product.id} className="admin-product-item" onClick={() => setSelectedProduct(product)}>
-          <h1 className="product-name">{product.name}</h1>
-          <h1 className="product-price">${product.price}</h1>
+            <div key={product.id} className="bg-slate-200 cursor-pointer p-4 m-1 w-full transition-transform duration-300 transform hover:scale-105" onClick={() => setSelectedProduct(product)}>
+          <h1 className="text-lg font-bold inline">{product.name}</h1>
+          <h1 className="text-right float-right inline">${product.price}</h1>
             </div>
           ))}
           <ProductOverlay product={selectedProduct} onClose={() => setSelectedProduct(null)} />
