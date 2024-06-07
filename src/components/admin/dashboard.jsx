4caa9d517bf4830588a9146f8759df4a22804e4f
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-06-07 21:27:35 +1200

    Refactored admin product list layout.

--- src/components/admin/dashboard.jsx
+++ src/components/admin/dashboard.jsx
@@ -1,11 +1,8 @@
-// AdminDashboard.jsx
 import React, { useState } from 'react';
-import '../../css/admin.css';
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase-config";
 import { useEffect } from 'react';
 import AdminProductList from './ProductList'
-import { Link } from 'react-router-dom';
 
 const AdminDashboard = () => {
 
@@ -21,51 +18,15 @@ const AdminDashboard = () => {
   }, []);
 
 
-  const [activeTab, setActiveTab] = useState('dashboard');
-
-  const handleTabChange = (tab) => {
-    setActiveTab(tab);
-  };
-
   return (
-    <div className="admin-dashboard font-montserrat">
-      <div className="sidebar">
-        <h2 className='text-xl font-bold'>Admin Panel</h2>
-        <div className="tabs">
-          <div
-            className={`tab ${activeTab === 'dashboard' ? 'active' : ''}`}
-            onClick={() => handleTabChange('dashboard')}
-          >
-            Dashboard
-          </div>
-        <div
-          className={`tab ${activeTab === 'products' ? 'active' : ''}`}
-          onClick={() => handleTabChange('products')}
-        >
-            Products
+    <div className="font-montserrat">
+        <div>
+           <AdminProductList />
         </div>
-          <div
-            className={`tab ${activeTab === 'orders' ? 'active' : ''}`}
-            onClick={() => handleTabChange('orders')}
-          >
-            Orders
-          </div>
-        </div>
-      </div>
-      <div className="main-content">
-        <h1 className='title text-xl font-bold'>Admin Dashboard</h1>
-        <div className="tab-content">
-          {activeTab === 'dashboard' && <DashboardContent />}
-          {activeTab === 'products' && <AdminProductList />}
-          {activeTab === 'orders' && <OrdersContent />}
-        </div>
-      </div>
+        
     </div>
   );
 };
-
-const DashboardContent = () =>  <div>Dashboard Content Goes Here</div>;
-const OrdersContent = () => <div>Orders Content Goes Here</div>;
 
 
 
