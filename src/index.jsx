39f7907d6525bb55d1dc7cb8204b4d66250d7dfc
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-06-08 11:32:34 +1200

    changed render in index.js.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; 
-import { HashRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <HashRouter>
-        <App />
-    </HashRouter>
-);
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
