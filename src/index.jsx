cbd67d421a600ec654e7bc90c6452880881a5d73
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-06-08 11:00:25 +1200

    Fixed error in index.jsx.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; 
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <HashRouter>
+        <App />
+    </HashRouter>
 );
