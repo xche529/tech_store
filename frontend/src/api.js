4a4a9b59a2e95e1793e88a9601edffea38663767
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-06-25 18:27:14 +1200

    Added method for calling cloud function from firebase backend.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,6 +5,18 @@ export const fetchProducts = async () => {
   return response.data;
 };
 
+export const fetchProductsByTag = async (keyWords) => {
+    try {
+      const response = await axios.get('https://us-central1-tech-store-68146.cloudfunctions.net/getProductsByTags', {
+        params: { tags: keyWords.join(" ") }
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      return [];
+    }
+  }
+
 
 
 
