2b05ff6949b167d32f10f79bd81d43457b428dcf
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-06-25 18:53:36 +1200

    Added getItemById endpoint and config.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -17,6 +17,18 @@ export const fetchProductsByTag = async (keyWords) => {
     }
   }
 
+  export const getProductById = async (itemId) => {
+    try {
+      const response = await axios.get(' https://us-central1-tech-store-68146.cloudfunctions.net/geProductById', {
+        params: { itemId }
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching item:", error);
+      return null;
+    }
+  };
+
 
 
 
