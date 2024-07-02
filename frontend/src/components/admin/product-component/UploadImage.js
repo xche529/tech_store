87ebae5db9e85012bc61a89aa7b682530d6e1ef3
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-07-02 17:16:02 +1200

    feat: improved createNewItem functionality.

--- frontend/src/components/admin/product-component/UploadImage.js
+++ frontend/src/components/admin/product-component/UploadImage.js
@@ -2,23 +2,18 @@ import { storage } from '../../../firebase-config';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 const UploadImage = async ({ file }) => {
-    if (!file) return null; // Check if file exists
+    if (!file) return null; 
 
     try {
-        // Create a reference to the storage location
         const storageRef = ref(storage, `product_images/${file.name}`);
-
-        // Upload the file to the storage location
         const snapshot = await uploadBytes(storageRef, file);
         console.log('Uploaded a file:', snapshot);
-
-        // Get the download URL for the file
         const url = await getDownloadURL(storageRef);
         console.log('File available at:', url);
-        return url; // Return the download URL
+        return url; 
     } catch (error) {
         console.error('Error uploading file:', error);
-        return null; // Return null on error
+        return null; 
     }
 };
 
