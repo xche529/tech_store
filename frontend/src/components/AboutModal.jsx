3f2acd235af448c9680f3138133a4b8b70111953
James Sun <126377149+y-sun311@users.noreply.github.com>
2024-08-13 00:40:02 +1200

    Implemented responsive header; changed layout.

--- frontend/src/components/AboutModal.jsx
+++ frontend/src/components/AboutModal.jsx
@@ -1,58 +1,65 @@
-import React, { useRef } from "react";
-
+import React from "react";
 
 const AboutModal = ({ closeAbout }) => {
-
   return (
     <div
       onClick={closeAbout}
       className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50"
     >
-      <div className="w-screen h-screen flex justify-center items-center align-center">
+      <div className="w-screen h-screen flex justify-center items-center">
         <div
-          className="border-2 bg-white flex flex-col text-center p-4 rounded-lg max-w-[80vw]"
+          className="border-2 bg-white flex flex-col text-center p-6 rounded-lg max-w-[90vw] md:max-w-[60vw] lg:max-w-[40vw] shadow-lg"
+          onClick={(e) => e.stopPropagation()} // Prevents the modal from closing when clicking inside the content area
         >
-          <h2>About KIWI TECH</h2>
-          <br />
-          <p>KIWI TECH is a project built by a team of 3.</p>
-          <p>
-            The project is built on React for Frontend, Firebase for Backend,
-            and TailwindCSS for styling.
+          <h2 className="text-2xl font-semibold mb-4">About The Project</h2>
+          <p className="text-gray-700 mb-4">
+            KIWI TECH is a e-commerce website project that sells a variety of tech products.
           </p>
-          <br />
+          <p className="text-gray-700 mb-6">
+            The project is built on React, Firebase Cloud Functions and TailwindCSS.
+          </p>
           <a
             href="https://github.com/xche529/tech_store"
             target="_blank"
             rel="noreferrer"
-            className="text-blue-500"
+            className="text-blue-600 font-medium hover:underline mb-8"
           >
-            Repository Link
+            GitHub
           </a>
-          <hr />
-          <h2>Contributors</h2>
-          <div className="flex align-center justify-center items-center gap-1">
-          <a
+          <hr className="border-t-2 border-gray-300 mb-6" />
+          <h2 className="text-xl font-semibold mb-4">Contributors</h2>
+          <div className="flex justify-center gap-4 mb-4">
+            <a
               href="https://github.com/xche529"
               target="_blank"
-              rel="noreferrer" className="text-blue-500"
+              rel="noreferrer"
+              className="text-blue-600 font-medium hover:underline"
             >
               Tommy
-            </a>{" "}
+            </a>
             <a
               href="https://github.com/y-sun311"
               target="_blank"
-              rel="noreferrer" className="text-blue-500"
+              rel="noreferrer"
+              className="text-blue-600 font-medium hover:underline"
             >
               James
-            </a>{" "}
+            </a>
             <a
               href="https://github.com/Ruotong-Betty-Zhang"
               target="_blank"
-              rel="noreferrer" className="text-blue-500"
+              rel="noreferrer"
+              className="text-blue-600 font-medium hover:underline"
             >
-             Betty
+              Betty
             </a>
           </div>
+          <button
+            onClick={closeAbout}
+            className="mt-4 py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors duration-200"
+          >
+            Close
+          </button>
         </div>
       </div>
     </div>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faStore, faHeart, faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { useAuth} from "../context/authContext";
+import { faUser, faStore, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "../context/authContext";
 import { SearchBar } from "./SearchBar";
 import LogIn from "./account/LogIn";
 import Cart from "./shoppingCart/Cart";
@@ -11,15 +11,13 @@ import { useCart } from '../context/cartContext';
 import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth';
 
 function Header() {
-const location = useLocation();
+  const location = useLocation();
   const navigate = useNavigate();
   const auth = getAuth();
-  const {cartItems} = useCart();
+  const { cartItems } = useCart();
   const { logout } = useAuth();
 
-
-
-  const [totalPrice, setTotalPrice] = useState(0); // State to hold total price
+  const [totalPrice, setTotalPrice] = useState(0); 
   const { user, login } = useAuth();
   const [isLoginOverlayOpen, setLoginOverlayOpen] = useState(false);
   const [isCartOverlayOpen, setCartOverlayOpen] = useState(false);
@@ -57,14 +55,13 @@ const location = useLocation();
     setLoginOverlayOpen(!isLoginOverlayOpen);
   };
 
-const setAboutOpen = () => {
+  const setAboutOpen = () => {
     setAboutOverlayOpen(true);
-    }
+  }
 
- const setAboutClose = () => {
+  const setAboutClose = () => {
     setAboutOverlayOpen(false);
-    }
-
+  }
 
   const setCartOpen = () => {
     setCartOverlayOpen(true);
@@ -83,61 +80,66 @@ const setAboutOpen = () => {
   const handleSignOut = async () => {
     try {
       await signOut(auth);
-     logout();
+      logout();
       navigate('/');
     } catch (error) {
       console.error('Sign out error:', error);
     }
   };
+
   return (
-    <div className="flex flex-col item-center mb-4">
-      <div>
-        <div className="my-10 flex justify-between">
-          <div className="ml-10">
-            <Link to="/">
-              <h1 className="font-bold text-5xl">KIWITECH</h1>
+    <div className="flex flex-col items-center mb-4">
+      <div className="w-full px-4 lg:px-20">
+        <div className="my-10 flex flex-col lg:flex-row justify-between items-center">
+          <div className="flex justify-between w-full lg:w-auto">
+            <Link to="/" className="flex items-center">
+              <h1 className="font-bold text-3xl lg:text-5xl">KIWITECH</h1>
             </Link>
+            <div className="lg:hidden">
+              <button
+                onClick={toggleLoginOverlay}
+                className="px-5 py-4 bg-gradient-to-r from-purple-500 to-blue-700 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg ml-4"
+              >
+                Account <FontAwesomeIcon icon={faUser} />
+              </button>
+            </div>
           </div>
 
-          <div className="flex space-x-4">
-          {userEmail ? (
-              <div className="flex items-center">     
+          <div className="flex space-x-4 items-center mt-4 lg:mt-0">
+            {userEmail ? (
+              <div className="flex items-center">
                 <button
                   onClick={handleSignOut}
                   className="px-5 py-4 bg-gradient-to-r from-purple-500 to-blue-700 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg"
                 >
-                <span className="text-green-400 font-bold">
-                  Welcome, {userEmail.slice(0,5)}!  
-                </span>
+                  <span className="text-green-400 font-bold">
+                    Welcome, {userEmail.slice(0, 5)}!
+                  </span>
                   Sign Out
                 </button>
               </div>
             ) : (
               <button
                 onClick={toggleLoginOverlay}
-                className="px-5 py-4 bg-gradient-to-r from-purple-500 to-blue-700 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg"
+                className="hidden lg:block px-5 py-4 bg-gradient-to-r from-purple-500 to-blue-700 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg"
               >
                 Account <FontAwesomeIcon icon={faUser} />
               </button>
             )}
             <Link to="/admin">
-              <button className="hidden sm:block px-5 py-4 bg-gradient-to-r from-purple-500 to-blue-700 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg">
+              <button className="hidden lg:block px-5 py-4 bg-gradient-to-r from-purple-500 to-blue-700 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg">
                 Admin
               </button>
             </Link>
             <button onClick={setAboutOpen} className="hidden md:block px-5 py-4 bg-gradient-to-r from-purple-500 to-blue-700 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg">
               About <FontAwesomeIcon icon={faStore} />
             </button>
-          </div>
-
-          <div className="mr-20 space-x-4">
-
             <button
               onClick={setCartOpen}
-              class="px-3 py-3 bg-black text-white font-bold rounded-2xl transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg"
-             >
+              className="px-3 py-3 bg-black text-white font-bold rounded-2xl transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg ml-4 lg:ml-0"
+            >
               <FontAwesomeIcon icon={faCartShopping} size="2x" />
-              ${totalPrice.toFixed(2)}
+              <span className="ml-2">${totalPrice.toFixed(2)}</span>
             </button>
           </div>
         </div>
@@ -146,9 +148,9 @@ const setAboutOpen = () => {
         </div>
       </div>
       {isLoginOverlayOpen && (
-            <LogIn closeLogin={setLoginClose}/>
+        <LogIn closeLogin={setLoginClose} />
       )}
-      {isCartOverlayOpen && <Cart closeCart={setCartClose} userDetail={user}/>}
+      {isCartOverlayOpen && <Cart closeCart={setCartClose} userDetail={user} />}
       {isAboutOverlayOpen && <About closeAbout={setAboutClose} />}
     </div>
   );
